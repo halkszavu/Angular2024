ba254a170aea394297f9560b1b0bd819bd18fc16
halkszavu <halkszavu@outlook.com>
2024-10-11 15:47:57 +0200

    scaffolded directives

---
+++ src/app/directives/team-button.directive.spec.ts
@@ -0,0 +1,8 @@
+import { TeamButtonDirective } from './team-button.directive';
+
+describe('TeamButtonDirective', () => {
+  it('should create an instance', () => {
+    const directive = new TeamButtonDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/directives/team-button.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appTeamButton]',
+  standalone: true
+})
+export class TeamButtonDirective {
+
+  constructor() { }
+
+}
