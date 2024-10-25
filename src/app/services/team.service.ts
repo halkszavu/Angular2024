8a8f2d1be8a436cb38cb94f71863a7e7edabda20
halkszavu <halkszavu@outlook.com>
2024-10-25 14:41:26 +0200

    http Service provided and consumed

--- src/app/services/team.service.ts
+++ src/app/services/team.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { Team } from '../teams/model/teams.model';
 import { TEAMS } from '../teams/model/team-list';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeamService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
   
   getTeams(): Observable<Team[]>{
     return of(TEAMS);
